999f35d0760f30598851ee74928076aeb2587ae7
clucastefan <clucastefan@yahoo.com>
2021-05-21 17:25:47 +0300

    users bash

---
+++ ToolAutomatization/share_bash/share_users.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+command="$1"
+echo "Utilizatorii enumerati : [user] [rid]" > SCANS/$command/share_users
+echo "" >> SCANS/$command/share_users
+echo "$(cat SCANS/10.10.254.71/share_enum | grep -w "user:")" >> SCANS/$command/share_users
