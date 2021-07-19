5a3f4790457980853028ceea2fabf65972a0995c
clucastefan <clucastefan@yahoo.com>
2021-07-19 20:29:10 +0300

    implementare login + signup

--- WebApplication/frontend/src/main/pages/Auth.js
+++ WebApplication/frontend/src/main/pages/Auth.js
@@ -6,12 +6,16 @@ import Button from '../../shared/components/FormElements/Button'
 import {VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE} from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
 import { AuthContext } from '../../shared/context/auth-context';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import './Auth.css'
 
 const Auth = () => {
     const auth = useContext(AuthContext);
 
     const [isLoginMode, setIsLoginMode] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError]  = useState(false);
 
     const [formState, inputHandler, setFormData] = useForm({
         email: {
@@ -43,13 +47,70 @@ const Auth = () => {
         setIsLoginMode(prevMode => !prevMode);
     };
 
-    const authSubmiteHandler = event => {
+    const authSubmiteHandler = async event => {
         event.preventDefault();
-        console.log(formState.inputs);
-        auth.login();
+
+        setIsLoading(true);
+        
+        if(isLoginMode){
+            try {
+                const response = await fetch('http://localhost:5000/api/users/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: formState.inputs.email.value,
+                    password: formState.inputs.password.value
+                    })
+                });
+
+                const responseData = await response.json();
+                if(!response.ok){
+                    throw new Error(responseData.message);
+                }
+                setIsLoading(false);       
+                auth.login();
+            } catch (err){
+                setIsLoading(false);
+                setError(err.message || 'A aparut o eroare. Incearca din nou');
+            }
+        } else {
+
+            try {
+                const response = await fetch('http://localhost:5000/api/users/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: formState.inputs.name.value,
+                    email: formState.inputs.email.value,
+                    password: formState.inputs.password.value
+                    })
+                });
+
+                const responseData = await response.json();
+                if(!response.ok){
+                    throw new Error(responseData.message);
+                }
+                setIsLoading(false);       
+                auth.login();
+            } catch (err){
+                setIsLoading(false);
+                setError(err.message || 'A aparut o eroare. Incearca din nou');
+            }
+        }
     };
 
-    return ( <Card className="authentication">
+    const errorHandler = () => {
+        setError(null);
+    };
+
+    return ( <React.Fragment>
+        <ErrorModal error={error} onClear={errorHandler}/>
+        <Card className="authentication">
+        {isLoading && <LoadingSpinner asOverlay/>}
         <h2>Login required</h2>
         <hr />
         <form onSubmit={authSubmiteHandler}>
@@ -86,7 +147,8 @@ const Auth = () => {
             <Button danger onClick={switchModeHandler}>
                 SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
             </Button>
-        </Card> );
+        </Card>
+        </React.Fragment> );
 };
 
 export default Auth;
